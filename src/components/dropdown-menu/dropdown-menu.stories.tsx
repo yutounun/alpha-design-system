7cbb4be016a4d129e9f67609cd3f5fcfe89bafda
yutounun <geek.yuto@gmail.com>
2026-04-18 14:05:11 -0700

    dropdown review

--- src/components/dropdown-menu/dropdown-menu.stories.tsx
+++ src/components/dropdown-menu/dropdown-menu.stories.tsx
@@ -1,14 +1,20 @@
+import * as React from "react"
 import type { Meta, StoryObj } from "@storybook/react-vite"
+import { Mail, Settings, User } from "lucide-react"
 import { expect, userEvent, within } from "storybook/test"
 
 import { Button } from "@/components/button"
 
 import {
     DropdownMenu,
+    DropdownMenuCheckboxItem,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from "./dropdown-menu"
 
@@ -50,5 +56,77 @@ export const Default: Story = {
                 doc.getByRole("menuitem", { name: "Profile" })
             ).toBeInTheDocument()
         })
+    },
+}
+
+export const InsetWithCheckboxes: Story = {
+    render: () => {
+        const [statusBar, setStatusBar] = React.useState(true)
+        const [push, setPush] = React.useState(false)
+
+        return (
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline">Open menu</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel inset>View</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem
+                        checked={statusBar}
+                        onCheckedChange={setStatusBar}
+                    >
+                        Show status bar
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                        checked={push}
+                        onCheckedChange={setPush}
+                    >
+                        Push notifications
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuLabel inset>Other</DropdownMenuLabel>
+                    <DropdownMenuItem inset>
+                        Keyboard shortcuts…
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        )
+    },
+}
+
+export const Submenu: Story = {
+    render: () => {
+        return (
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline">Open menu</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Layout</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Compact density</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <User />
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                            <Mail />
+                            <span>Invite</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuSubContent>
+                            <DropdownMenuItem>Email</DropdownMenuItem>
+                            <DropdownMenuItem>Message</DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                    </DropdownMenuSub>
+                    <DropdownMenuItem>
+                        <Settings />
+                        Settings
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        )
     },
 }
